2143d0c2111c8d2d349a055feceb9dd6f620b582
nekodayo2222 <99164438+nekodayo2222@users.noreply.github.com>
2023-11-07 22:36:27 +0900

    first commit

---
+++ mdesign.js
@@ -0,0 +1,4 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/select/outlined-select.js';
+import '@material/web/select/select-option.js';
+import '@material/web/dialog/dialog.js';
